641f6fce62fb95dd9f9e595efaa9d8c4862bb1dc
kaixuan1992 <2568189783@qq.com>
2019-05-19 22:43:22 +0800

    add examples

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 //生成脚本引入<script src="./dist/lib/index.js"></script>
 module.exports = Object.assign({}, base, {
     mode: 'development',
+    entry:{
+        example:'./example.tsx'
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'example.html'
         })
     ],
 })
