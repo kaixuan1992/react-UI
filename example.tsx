d7f3f6abc531e845a672c07bdf59595563c13e4d
kaixuan1992 <2568189783@qq.com>
2019-05-21 22:04:58 +0800

    add simply dialog

--- example.tsx
+++ example.tsx
@@ -1,9 +1,11 @@
-import Icon from './lib/icon/icon';
+// import Icon from './lib/icon/icon';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router,Route,Link} from 'react-router-dom'
 import IconExample from './lib/icon/icon.example'
 import ButtonExample from './lib/button.example'
+import DialogExample from './lib/dialog/dialog.example'
+
 
 ReactDOM.render((
   <Router>
@@ -21,11 +23,15 @@ ReactDOM.render((
           <li>
             <Link to="/button">Button</Link>
           </li>
+          <li>
+            <Link to="/dialog">Dialog</Link>
+          </li>
         </ul>
       </aside>
       <main>
           <Route path="/icon" component={IconExample}></Route>
           <Route path="/button" component={ButtonExample}></Route>
+          <Route path="/dialog" component={DialogExample}></Route>
       </main>
     </div>
   </div>
